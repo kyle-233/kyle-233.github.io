d68e9c29b39f99b359938b4607583e774d307bbd
kyle-233 <kyle07142113@163.com>
2023-09-21 15:11:15 +0800

    fix:路由问题

--- src/router/page.tsx
+++ src/router/page.tsx
@@ -3,7 +3,7 @@ import { Suspense, lazy } from "react"
 
 const getPage = (name: string): JSX.Element => {
 
-    const Content = lazy(() => import(/* @vite-ignore */`../pages/${name}`))
+    const Content = lazy(() => import(/* @vite-ignore */`../../src/pages/${name}`))
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Content />
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   resolve: {
     alias: {
+      '@': 'src',
       'pages': 'src/pages',
       'request': 'src/request'
     },
