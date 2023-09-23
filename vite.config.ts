7dae06dbe6a867e939b41531873a8938a19ae4bd
kyle-233 <kyle07142113@163.com>
2023-09-23 10:45:58 +0800

    feat:build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer as rollupPluginVisualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +12,15 @@ export default defineConfig({
       'request': 'src/request'
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+    rollupPluginVisualizer({ open: true })
+  ],
 })
